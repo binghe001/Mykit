69e87bba8ba4ce3f24c00899118df92e02a63c79
liuyazhuang <liuyazhuang@DESKTOP-QLR4DIE>
2018-05-10 14:36:28 +0800

    消除警告

--- mykit-tag/mykit-tag-spring/mykit-tag-spring-customer/src/test/java/io/mykit/tag/spring/customer/Main.java
+++ mykit-tag/mykit-tag-spring/mykit-tag-spring-customer/src/test/java/io/mykit/tag/spring/customer/Main.java
@@ -10,6 +10,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @author liuyazhuang
  *
  */
+@SuppressWarnings("resource")
 public class Main {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("lyz-element.xml");
